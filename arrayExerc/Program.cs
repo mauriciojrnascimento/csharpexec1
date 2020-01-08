9996b39a63eeb92da6b53e406c5e040b94814601
Mauricio Nascimento <mauriciojr.nascimento@gmail.com>
2020-01-08 09:18:17 -0300

    Concluídos exercícios

---
+++ arrayExerc/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Exercicios
+{
+    
+    class Program
+    {
+        public static int[] Numeros = new int[] {2,1,3,4,6,5,7,8,9,10,12,11,13,15,14,16,17,20,19,18};
+        public static int[] NumerosPrimo = 
+            new int[] {
+                        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 
+                        103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 
+                        211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 
+                        331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449};
+
+        public static int[] ArrayNumeros = new int[20];
+        static void Main(string[] args)
+        {   
+            
+            OrdenaCrescente(Numeros);            
+            imprimeArray(Numeros);
+            OrdenaDecrescente(Numeros);
+            imprimeArray(Numeros);
+            SomentePares(Numeros); 
+            SomenteImpares(Numeros);
+            SomentePrimos(Numeros);
+            
+        }
+
+        static Array GeraArray(int qtd){
+            int[] array = new int[qtd];
+            return array;
+        }
+
+        static void OrdenaCrescente(int[] array){
+            for(int i = 1; i < array.Length; i++ )
+                for(int j = 0; j < array.Length-1; j++)
+                    if(array[j] > array[j+1])
+                        Troca(array,j);           
+        }
+
+        static void OrdenaDecrescente(int[] array){
+            for(int i = 1; i < array.Length; i++ )
+                for(int j = 0; j < array.Length-1; j++)
+                    if(array[j] < array[j+1])
+                        Troca(array,j);                    
+                
+        }
+        static void Troca(int[] array, int primeiro){
+            int temporaria;
+            temporaria = array[primeiro];
+            array[primeiro] = array[primeiro + 1];
+            array[primeiro + 1] = temporaria;
+        }
+
+        static void SomentePares(int[] array){
+            int index = 0;
+            for(int i = 0; i < array.Length; i++){                
+                if(EhPar(array[i])){
+                    ArrayNumeros[index] = array[i];        
+                    AtualizaTamanhoArray(ref ArrayNumeros, ArrayNumeros.Length + 1);
+                    Console.WriteLine(ArrayNumeros[index]);
+                    index++;
+                }                
+            }
+        }
+
+        static void AtualizaTamanhoArray(ref int[] array, int tamanho){
+            Array.Resize(ref array, tamanho + 1);            
+        }
+
+        static bool EhPar(int valor){
+
+            return valor%2 == 0;
+
+        }
+
+        static void SomenteImpares(int[] array){
+            int index = 0;
+            for(int i = 0; i < array.Length; i++){                
+                if(!EhPar(array[i])){
+                    ArrayNumeros[index] = array[i];        
+                    Array.Resize(ref ArrayNumeros, ArrayNumeros.Length + 1);
+                    Console.WriteLine(ArrayNumeros[index]);
+                    index++;                    
+                }
+            } 
+        }
+
+        static void SomentePrimos(int[] array){
+            int index = 0;
+            for(int i = 0; i < array.Length; i++){                
+                if(ValidaNumeroPrimo(array[i])){
+                    ArrayNumeros[index] = array[i];        
+                    Array.Resize(ref ArrayNumeros, ArrayNumeros.Length + 1);
+                    Console.WriteLine(ArrayNumeros[index]);
+                    index++;                    
+                }
+            } 
+        }
+
+        static bool ValidaNumeroPrimo(int numero){
+            if(numero != 1)
+                for(int i = 2; i <= numero; i++){
+                    if(numero % i > 0) 
+                        return true;
+                    else 
+                        return false;   
+                }                             
+            return false;
+        }
+
+        static void imprimeArray(int[] array){
+            for(int i = 0; i < array.Length; i++)
+                Console.WriteLine(array[i]);
+                
+            Console.WriteLine("########");
+        }
+
+    }
+}
---
+++ equacaoExerc/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace equacaoExerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {               
+            EquacaoPrimeiroGrau();
+        }
+
+        static void EquacaoSegundoGrau(){
+            Console.WriteLine("Digite A, B e C para Fazer uma Equação de Segundo Grau.");
+            Console.Write("A: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("B: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("C: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double delta = b * b - 4 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+        }
+
+        static void EquacaoPrimeiroGrau(){
+            Console.WriteLine("Digite A e B para Fazer uma Equação de Primeiro Grau(ax + b = 0).");
+            Console.Write("A: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("B: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Equação:{a}x + {b} = 0");
+
+            double x = (-1 * b) / a;
+            Console.WriteLine(x);            
+
+        }
+    }
+}
